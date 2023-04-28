79ce16c32ada2567ee766e1bd98d7f3f34803ade
avocadojesus <jgarbey@gmail.com>
2023-04-28 00:05:53 -0700

    update styles a bit

--- src/app/ui/code-example.tsx
+++ src/app/ui/code-example.tsx
@@ -1,9 +1,9 @@
-import { qtcreatorDark as lightTheme } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { far as lightTheme } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 // import { far as lightTheme } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 // import { idea as lightTheme } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import { darcula as darkTheme } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { Inspector } from "react-inspector";
+import { darcula as darkTheme } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { Inspector } from 'react-inspector'
 
 export default function CodeExample({
   id,
@@ -13,35 +13,32 @@ export default function CodeExample({
   children,
   description = <div></div>,
   expandLevel = 0,
-  theme = "light",
-  language = "javascript",
+  theme = 'light',
+  language = 'javascript',
 }: {
-  id: string;
-  title?: string;
-  codeExample: string;
-  results?: any;
-  children?: React.ReactNode;
-  description?: React.ReactNode;
-  expandLevel?: number;
-  theme?: "light" | "dark";
-  language?: string;
+  id: string
+  title?: string
+  codeExample: string
+  results?: any
+  children?: React.ReactNode
+  description?: React.ReactNode
+  expandLevel?: number
+  theme?: 'light' | 'dark'
+  language?: string
 }) {
-  let selectedTheme: any;
+  let selectedTheme: any
   switch (theme) {
-    case "light":
-      selectedTheme = lightTheme;
-      break;
+    case 'light':
+      selectedTheme = lightTheme
+      break
 
-    case "dark":
-      selectedTheme = darkTheme;
-      break;
+    case 'dark':
+      selectedTheme = darkTheme
+      break
   }
 
   return (
-    <div
-      className={`code-example ${theme} ${!title ? " reduced" : ""}`}
-      id={id}
-    >
+    <div className={`code-example ${theme} ${!title ? ' reduced' : ''}`} id={id}>
       <div className="notation-title">{title && <h3>{title}</h3>}</div>
 
       <div className="description">{description}</div>
@@ -58,20 +55,15 @@ export default function CodeExample({
         <div className="notation-transcription code">
           {results && (
             <div className="code-wrap">
-              {typeof results === "string" ? (
+              {typeof results === 'string' ? (
                 <pre className="results">{results}</pre>
               ) : (
-                <Inspector
-                  theme="chromeDark"
-                  expandLevel={expandLevel}
-                  table={false}
-                  data={results}
-                />
+                <Inspector theme="chromeDark" expandLevel={expandLevel} table={false} data={results} />
               )}
             </div>
           )}
         </div>
       </div>
     </div>
-  );
+  )
 }
