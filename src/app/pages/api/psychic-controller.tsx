0bb15b6383e231ce352b8761cafd5baf1a1e8168
Jamie Clark <jclark@rvohealth.com>
2024-03-21 22:23:30 -0400

    Remove references to unused code

--- src/app/pages/api/psychic-controller.tsx
+++ src/app/pages/api/psychic-controller.tsx
@@ -1,37 +1,32 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import APILeftSidebar from "../../ui/api/left-sidebar";
-import CodeExample from "../../ui/code-example";
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import APILeftSidebar from '../../ui/api/left-sidebar'
 
 export default function APIPsychicControllerPage() {
-  const location = useLocation();
+  const location = useLocation()
   useEffect(() => {
-    if (location.hash) document.querySelector(location.hash)?.scrollIntoView();
-  }, [location.hash]);
+    if (location.hash) document.querySelector(location.hash)?.scrollIntoView()
+  }, [location.hash])
 
   return (
     <div className="page api psychic-controller">
-      <APILeftSidebar expanded={["psychic-controller"]} />
+      <APILeftSidebar expanded={['psychic-controller']} />
 
       <div className="docs api">
         <div className="documentation">
           <h1>PsychicController</h1>
 
           <p>
-            The PsychicController class represents the <i>C</i> in the{" "}
-            <i>MVC</i> pattern. Any class&nbsp; extending a PsychicController
-            should live in the <span className="hl">app/controllers</span>&nbsp;
-            folder, and should have routes pointing to the public method
-            definitions you customize upon it.
+            The PsychicController class represents the <i>C</i> in the <i>MVC</i> pattern. Any class&nbsp;
+            extending a PsychicController should live in the <span className="hl">app/controllers</span>&nbsp;
+            folder, and should have routes pointing to the public method definitions you customize upon it.
           </p>
 
           <p>
-            The primary purpose of the PsychicController is to collect params
-            from an http request, send them&nbsp; to some underlying service,
-            and then render a response. Controller logic shouldn't be too
-            expansive,&nbsp; since the nature of each method is to be called by
-            a single endpoint, making the methods themselves&nbsp; naturally
-            unsharable with other services.
+            The primary purpose of the PsychicController is to collect params from an http request, send
+            them&nbsp; to some underlying service, and then render a response. Controller logic shouldn't be
+            too expansive,&nbsp; since the nature of each method is to be called by a single endpoint, making
+            the methods themselves&nbsp; naturally unsharable with other services.
           </p>
 
           <h2>Properties</h2>
@@ -56,5 +51,5 @@ export default function APIPsychicControllerPage() {
         </div>
       </div>
     </div>
-  );
+  )
 }
